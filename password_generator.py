5bc7cfb5d7f116459b2ec1c8c3b81b1619434ba6
Bushraabir <muzahidulislamabir123@gmail.com>
2025-08-11 13:42:57 +0600

    Add secrets module and character diversity to password generator

--- password_generator.py
+++ password_generator.py
@@ -1,24 +1,32 @@
 import string
 import random
+import secrets ## built-in Python module for secure random
 
 def generate_password(length: int) -> str:
-    """Generate a random password of given length."""
-    upper = list(string.ascii_uppercase)
-    lower = list(string.ascii_lowercase)
-    digit = list(string.digits)
-    punct = list(string.punctuation)
+    """Generate a cryptographically secure random password of given length."""
 
-    # Shuffle lists (optional, random.choices is already random)
-    random.shuffle(upper)
-    random.shuffle(lower)
-    random.shuffle(digit)
-    random.shuffle(punct)
-
+    upper = string.ascii_uppercase # String is direct compitable with secrets.choice
+                                   # And helps in space optimization
+    lower = string.ascii_lowercase
+    digit = string.digits
+    punct = string.punctuation
     all_chars = upper + lower + digit + punct
-
-    # Generate password by choosing random characters
-    password = ''.join(random.choices(all_chars, k=length))
-    return password
+    
+    # Ensure at least one character from each category
+    password = [
+        secrets.choice(upper),
+        secrets.choice(lower),
+        secrets.choice(digit),
+        secrets.choice(punct)
+    ]
+    
+    # Fill the remaining length with random characters
+    password += [secrets.choice(all_chars) for _ in range(length - 4)]
+    
+    # Shuffle to avoid predictable positions
+    secrets.SystemRandom().shuffle(password)
+    
+    return ''.join(password)
 
 def main():
     while True:
@@ -32,8 +40,13 @@ def main():
             if password_length <= 0:
                 print("Please enter a positive integer.")
                 continue
+            elif password_length < 4:
+                raise ValueError("Length must be at least 4 for diversity.")
+                continue
+
 
             pwd = generate_password(password_length)
+
             print(f"Your password is: {pwd}\n")
             download=input("Do you want to save the password as text file : ?(Yes/No)").strip() 
             if download.lower()== 'yes':
